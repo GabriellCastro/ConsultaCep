c76391d0cac2593bb6b9c21fd63bcde0a5bc21a3
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-03-18 02:48:30 -0400

    ConsultaCep

--- js/main.js
+++ js/main.js
@@ -1,26 +1,21 @@
-//sitema de cep com ajax e jquery
-
 function consultaCep(){
-    var cep = document.getElementById("cep").value
-    var url = "https://viacep.com.br/ws/"+ cep +"/json/"
-    console.log(url)
+    let cep = document.getElementById("cep").value;
+    let url = `https://viacep.com.br/ws/"${cep}"/json/`;
     $.ajax({
         url: url,
         type: "GET",
         success: function(response){
-            console.log(response)
             $("#logradouro").html(response.logradouro) 
             $("#complemento").html(response.complemento)
             $("#bairro").html(response.bairro)
             $("#localidade").html(response.localidade)
             $("#uf").html(response.uf)
-            
         }
     })
 }
             //essa e outra maneira de se fazer
             
-            //document.getElementById("logradouro").innerHTML = response.logradouro
+            // document.getElementById("logradouro").innerHTML = response.logradouro
             // document.getElementById("complemento").innerHTML = response.complemento
             // document.getElementById("bairro").innerHTML = response.bairro
             // document.getElementById("localidade").innerHTML = response.localidade
